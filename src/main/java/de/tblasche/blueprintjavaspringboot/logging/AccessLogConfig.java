93555688b0e7564366dee5308ca3ae69d6efc631
Torsten <tbcollect.github@web.de>
2024-10-19 23:38:07 +0200

    updated dependencies

--- src/main/java/de/tblasche/blueprintjavaspringboot/logging/AccessLogConfig.java
+++ src/main/java/de/tblasche/blueprintjavaspringboot/logging/AccessLogConfig.java
@@ -2,7 +2,7 @@ package de.tblasche.blueprintjavaspringboot.logging;
 
 import ch.qos.logback.access.tomcat.LogbackValve;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.ConfigurableTomcatWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +12,13 @@ public class AccessLogConfig {
 
   @Bean
   @ConditionalOnProperty(name = "server.tomcat.accesslog.enabled")
-  public WebServerFactoryCustomizer<TomcatServletWebServerFactory> accessLogCustomizer() {
+  public WebServerFactoryCustomizer<ConfigurableTomcatWebServerFactory> accessLogCustomizer() {
     return factory -> {
       LogbackValve logbackValve = new LogbackValve();
       logbackValve.setFilename(LogbackValve.DEFAULT_FILENAME);
       logbackValve.setQuiet(true);
       logbackValve.setAsyncSupported(true);
-      factory.addContextValves(logbackValve);
+      factory.addEngineValves(logbackValve);
     };
   }
 }
